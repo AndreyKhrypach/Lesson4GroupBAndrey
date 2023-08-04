1c1fd626bbc76b0583aa703010fccd26efd197f5
AndreyKhrypach <pacik78@gmail.com>
2023-08-04 21:14:35 +0300

    add aryphmethic options

--- Src/ProgramLesson4.java
+++ Src/ProgramLesson4.java
@@ -3,5 +3,42 @@ public class ProgramLesson4 {
         System.out.println("Hello git");
         System.out.println("hello git from Andrey");
         System.out.println("Hello git from our group!");
+
+//        int number = 45; // 1001 Значення, яке потрібно зашифрувати - в двійковій формі 101101
+//        int key = 102; //Ключ шифрування - в двійковій системі 1100110
+//        int encrypt = number ^ key; //Результатом буде число 1001011 або 75
+//        System.out.println("Зашифроване число: " +encrypt);
+//
+//        int decrypt = encrypt ^ key; // Результатом будет число 45
+//        System.out.println("Розшифроване число: " + decrypt);
+
+
+//        //Логічне множення &
+//        int a = 2; // 010
+//        int b = 5; // 101
+//        System.out.println(a&b); // result 0
+//
+//        int a1 = 4; // 100
+//        int b1 = 5; // 101
+//        System.out.println(a1&b1); // result 4
+
+//        //логічне додавання |
+//        int a = 2; //010
+//        String strA = Integer.toBinaryString(a);
+//        System.out.println(strA);
+//        int b = 5; // 101
+//        String strB = Integer.toBinaryString(b);
+//        System.out.println(strB);
+//
+//        String aB = Integer.toBinaryString(a|b);
+//
+//        System.out.println(a|b); // result 7 - 111
+//        System.out.println(aB);
+//
+//        int a1 = 4; //100
+//        int b1 = 5; //101
+//
+//        System.out.println(a1|b1); // result 5 - 101
+//        System.out.println(Integer.toBinaryString(a1|b1)); // result 5 - 101
     }
 }
