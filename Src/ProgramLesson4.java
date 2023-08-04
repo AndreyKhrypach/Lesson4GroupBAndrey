fd574326ecc8907504fbde2e079d3b9706352337
AndreyKhrypach <pacik78@gmail.com>
2023-08-04 20:45:00 +0300

    third message edited

--- Src/ProgramLesson4.java
+++ Src/ProgramLesson4.java
@@ -2,6 +2,6 @@ public class ProgramLesson4 {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("hello git from Andrey");
-        System.out.println("Hello git from our group");
+        System.out.println("Hello git from our group!");
     }
 }
